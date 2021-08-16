0f19046688833406cb481788ebb6ce900e4b75a5
shivaniraul <88359212+shivaniraul@users.noreply.github.com>
2021-08-16 12:48:18 +0530

    Add files via upload

---
+++ loggerr.py
@@ -0,0 +1,10 @@
+import logging
+print("python")
+logging.basicConfig(level=logging.DEBUG)
+logging.info('This is an info message')
+logging.warning("This is a message")
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
